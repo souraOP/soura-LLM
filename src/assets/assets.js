524edcebdff7c9e9b49673ff3f92a13eee185046
souraOP <sourasishmondal95@gmail.com>
2024-04-09 00:09:53 +0530

    Changed the Whole UI

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -11,6 +11,7 @@ import message_icon from './message_icon.png'
 import code_icon from './code_icon.png'
 import send_icon from './send_icon.png'
 import gemini_icon from './gemini_icon.png'
+import backG from './background-contour.jpg';
 
 export const assets = {
     history_icon,
@@ -25,5 +26,6 @@ export const assets = {
     message_icon,
     code_icon,
     send_icon,
-    gemini_icon
+    gemini_icon,
+    backG
 }
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,19 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
 import { Context } from '../../context/context'
 import Card from './card'
 
+const randomWords = ["Friend", "Nigga", "Nigger", "Homie", "Buddy", "Bitch", "Slave", "Gay", "Gandu", "Pal"];
+
 const Main = () => {
+
+    const [greetings, setGreetings] = useState('Human');
+
+    useEffect(() => {
+        const getRandomWords = () => randomWords[Math.floor(Math.random() * randomWords.length)];
+        setGreetings(getRandomWords());
+    }, []);
 
     const {onSent, recentPrompt, showResults, loading, resultData, input, setInput} = useContext(Context)
 
 
     return (
-        <div className='main'>
+        <div className='main background-texture'>
             <div className='nav'>
-                <p>Soura-LLM</p>
-                <img src="https://cdn.statically.io/gh/souraOP/soura-llm-assests/main/user_icon.png" alt='userIcon'/>
+                <p><a href='/' style={{textDecoration: "none", color: "#e3e3e3"}}>Soura-LLM</a></p>
+                <img src="https://cdn.statically.io/gh/souraOP/soura-llm-assests/main/user_icon.png" alt='soura'/>
             </div>
             <div className='main-container'>
 
@@ -22,7 +31,7 @@ const Main = () => {
                     ?<>
                         <div className='greet-user'>
                             <p>
-                                <span>Hello, Human!</span>
+                                <span>Hello, {greetings}!</span>
                             </p>
                             <p>
                                 How can I help you today ?
@@ -39,7 +48,7 @@ const Main = () => {
                     :
                     <div className='result'>
                         <div className="result-title">
-                            <img src="https://cdn.statically.io/gh/souraOP/soura-llm-assests/main/user_icon.png" alt='user-icon'/>
+                            <img src="https://cdn.statically.io/gh/souraOP/soura-llm-assests/main/user_icon.png" alt='soura'/>
                             <p>{recentPrompt}</p>
                         </div>
                         <div className="result-data">
@@ -64,7 +73,14 @@ const Main = () => {
                 
                 <div className='main-bottom'>
                     <div className='search-box'>
-                        <input type='text' onChange={(e) => setInput(e.target.value)} value={input} placeholder='Message Soura-LLM...' />
+                        <input type='text' onChange={(e) => setInput(e.target.value)} value={input} placeholder='Message Soura-LLM...' 
+                            onKeyDown={(e) => {
+                                if(e.key === 'Enter' && input.trim()){
+                                    e.preventDefault();
+                                    onSent();
+                                }
+                            }} 
+                        />
                         <div>
                             <img src={assets.gallery_icon} alt="gallery-icon" />
                             <img src={assets.mic_icon} alt="mic-icon" />
